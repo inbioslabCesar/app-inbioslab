4a65c77b169673f6b9e8058723b36a2b1c7617ee
cesarperezguimaraez <inbioslab@gmail.com>
2023-08-28 18:21:09 -0500

    imput search modificando width

--- src/componets/InputSearch.jsx
+++ src/componets/InputSearch.jsx
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 function InputSearch({ search, setSearch }) {
   return (
     <div>
-      <div className="w-1/2 h-10 p-2 rounded-lg shadow-lg border-none mb-5 mt-2 outline-none hover:shadow-2xl placeholder:text-gray-300 relative">
+      <div className="w-2/3 h-10 p-2 rounded-lg shadow-lg border-none mb-5 mt-2 outline-none hover:shadow-2xl placeholder:text-gray-300 relative">
         <FaSearch className=" absolute mt-1 text-zinc-400"/>
         <input
           type="text"
