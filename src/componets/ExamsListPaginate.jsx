da9975b49ae60f42bd72fb239867a48f9552326b
cesarperezguimaraez <inbioslab@gmail.com>
2023-08-31 09:49:19 -0500

    actualizando datos

--- src/componets/ExamsListPaginate.jsx
+++ src/componets/ExamsListPaginate.jsx
@@ -13,6 +13,8 @@ const ExamsListPaginate = ({
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
         </select>
         <p className="">Elementos por página</p>
       </div>
