7febc317598e572879d79c24c9732726c11540b9
cesarperezguimaraez <inbioslab@gmail.com>
2024-01-17 17:21:48 -0500

    integrando logo whatsapp

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -102,6 +102,7 @@ const Home = () => {
                 <a
                   href="#"
                   className="bg-sky-400 text-slate-50 w-max py-3 px-12 mx-4 rounded-full font-bold shadow-xl shadow-blue-950/30 lg:block">
+                  <i className="ri-whatsapp-line text-green-500 bg-white rounded-full text-lg mr-2 p-1"></i>
                   Contáctanos
                 </a>
               </ReactWhatsapp>
